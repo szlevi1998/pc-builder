06f5015f56441f7f435b3a9cd7aa087867e17d6c
szlevi1998 <sz.levi1998@gmail.com>
2022-04-13 19:35:15 +0200

    Pre Build grid is almost filled the whole content

--- src/components/PreBuilds.js
+++ src/components/PreBuilds.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Navbar} from "react-bootstrap";
 import NavBar from "../NavBar";
 import "../stylesheets/prebuilds.css";
 
@@ -8,30 +7,230 @@ function OpenPreBuilds() {
             <NavBar></NavBar>
             <h1 className="title-h1">Alap konfigok</h1>
             <section className="intel-section">Intel konfigurációk</section>
-            <section>
-                <div>
-                    <div className="row ">
-                        <div className="col-md-4 left-element">Belépő szint</div>
-                        <div className="col-md-4 middle-element">Alsóközép szint</div>
-                        <div className="col-md-4 right-element">Közép szint</div>
-                        <div className="col-md-4 left-element">Felsőközép szint</div>
-                        <div className="col-md-4 middle-element">Erős szint</div>
-                        <div className="col-md-4 right-element">Legerősebb szint</div>
-                    </div>
+            <div>
+                <div className="row">
+                    <div className="col-md-4 left-element">
+
+                        <div className="left-box">
+                            Belépő szint
+                            <p>(200000 Ft alatt)</p>
+                            <p>GIGABYTE A520M S2H</p>
+                            <p>AMD Ryzen 5 5600G</p>
+                            <p>Gyári hűtő</p>
+                            <p>Crucial Ballistix 16GB (2x8GB) DDR4</p>
+                            <p>Western Digital Blue 3D NAND 2.5 500 GB SSD</p>
+                            <p>Cooler Master Elite Knight K350</p>
+                            <p>Cooler Master Elite V3 600W</p>
+                            <h4> Ár:185000 FT</h4>
+                            <section>
+
+                            </section>
+                        </div>
+
                     </div>
 
-            </section>
+                    <div className="col-md-4 middle-element">
+                        <div className="middle-box">
+                            Alsóközép szint
+                            <p>(250000 Ft alatt)</p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <h4></h4>
+                            <section>
+                            </section>
+                        </div>
+                    </div>
+                    <div className="col-md-4 right-element">
+                        <div className="right-box">
+                            Közép szint
+                            <p>(400000 Ft alatt)</p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <h4></h4>
+                            <section>
+
+                            </section>
+                        </div>
+                    </div>
+                    <div className="col-md-4 left-element">
+                        <div className="left-box">
+                            Felsőközép szint
+                            <p>(600000 Ft alatt)</p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <h4></h4>
+                            <section>
+
+                            </section>
+                        </div>
+                    </div>
+                    <div className="col-md-4 middle-element">
+                        <div className="middle-box">
+                            Erős szint
+                            <p>(800000 Ft alatt)</p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <h4></h4>
+                            <section>
+
+                            </section>
+                        </div>
+                    </div>
+                    <div className="col-md-4 right-element">
+                        <div className="right-box">
+                            Legerősebb szint
+                            <p>(1-1.5 millió Ft környéke)</p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <h4></h4>
+                            <section>
+
+                            </section>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
             <section className="amd-section">AMD konfigurációk</section>
             <section>
                 <section>
                     <div>
                         <div className="row">
-                            <div className="col-md-4 left-element">Belépő szint</div>
-                            <div className="col-md-4 middle-element">Alsóközép szint</div>
-                            <div className="col-md-4 right-element">Közép szint</div>
-                            <div className="col-md-4 left-element">Felsőközép szint</div>
-                            <div className="col-md-4 middle-element">Erős szint</div>
-                            <div className="col-md-4 right-element">Legerősebb szint</div>
+                            <div className="col-md-4 left-element">
+
+                                <div className="left-box">
+                                    Belépő szint
+                                    <p>(200000 Ft alatt)</p>
+                                    <p>GIGABYTE A520M S2H</p>
+                                    <p>AMD Ryzen 5 5600G</p>
+                                    <p>Gyári hűtő</p>
+                                    <p>Crucial Ballistix 16GB (2x8GB) DDR4</p>
+                                    <p>Western Digital Blue 3D NAND 2.5 500 GB SSD</p>
+                                    <p>Cooler Master Elite Knight K350</p>
+                                    <p>Cooler Master Elite V3 600W</p>
+                                    <h4> Ár:185 000 FT</h4>
+                                    <section>
+
+                                    </section>
+                                </div>
+
+                            </div>
+
+                            <div className="col-md-4 middle-element">
+                                <div className="middle-box">
+                                    Alsóközép szint
+                                    <p>(250000 Ft alatt)</p>
+                                    <p>ASUS TUF GAMING B550M-PLUS</p>
+                                    <p>AMD Ryzen 5 5600G</p>
+                                    <p>ARCTIC Freezer i35 CO </p>
+                                    <p>Corsair VENGEANCE RGB PRO SL 16GB (2x8GB) DDR4 </p>
+                                    <p>Samsung 980 500 GB </p>
+                                    <p>Thermaltake Versa N24 </p>
+                                    <p>EVGA PS-700W </p>
+                                    <h4>Ár:238 000 FT</h4>
+                                    <section>
+                                    </section>
+                                </div>
+                            </div>
+                            <div className="col-md-4 right-element">
+                                <div className="right-box">
+                                    Közép szint
+                                    <p>(400000 Ft alatt)</p>
+                                    <p>MSI MAG B550 Tomahawk 51</p>
+                                    <p>AMD Ryzen 5 5600G 85</p>
+                                    <p>Alpenföhn Brocken ECO Advanced 13</p>
+                                    <p>Kingston FURY Beast 32GB (2x16GB) DDR4 58</p>
+                                    <p>Gainward Ghost GTX 1650 OC 4GB 87</p>
+                                    <p>Samsung 980 500 GB 23</p>
+                                    <p>Lian Li LANCOOL II Mesh 38</p>
+                                    <p>EVGA 700 GD 29</p>
+                                    <h4>Ár: 384 000 FT </h4>
+                                    <section>
+
+                                    </section>
+                                </div>
+                            </div>
+                            <div className="col-md-4 left-element">
+                                <div className="left-box">
+                                    Felsőközép szint
+                                    <p>(600000 Ft alatt)</p>
+                                    <p>MSI MPG X570 Gaming Edge WIFI  73</p>
+                                    <p>AMD Ryzen 5 5800X 130</p>
+                                    <p>ARCTIC Liquid Freezer II 360 38</p>
+                                    <p>G.SKILL Trident Z 32GB (2x16GB) DDR4 58</p>
+                                    <p>PowerColor Fighter Radeon RX 6600 XT 8GB 200</p>
+                                    <p>Samsung 970 EVO Plus 1TB M.2" 43</p>
+                                    <p>Lian Li LANCOOL II Mesh 38</p>
+                                    <p>EVGA 700 GD 29</p>
+                                    <h4>Ár: 595 000 FT</h4>
+                                    <section>
+
+                                    </section>
+                                </div>
+                            </div>
+                            <div className="col-md-4 middle-element">
+                                <div className="middle-box">
+                                    Erős szint
+                                    <p>(800000 Ft alatt)</p>
+                                    <p>ASUS ROG Strix X570-F Gaming 95</p>
+                                    <p>AMD Ryzen 5 5800X 130</p>
+                                    <p>Corsair iCUE H150i ELITE 360 70</p>
+                                    <p> Kingston FURY Renegade 32GB (2x16GB) DDR4 66</p>
+                                    <p>GIGABYTE GeForce VISION RTX 3070 Ti 8GB OC 328</p>
+                                    <p>Samsung 980 1TB M.2 39</p>
+                                    <p>be quiet! Pure Base 500DX 38</p>
+                                    <p>SilentiumPC Supremo FM2 750W Gold 38</p>
+                                    <h4>Ár: 793 000 FT</h4>
+                                    <section>
+
+                                    </section>
+                                </div>
+                            </div>
+                            <div className="col-md-4 right-element">
+                                <div className="right-box">
+                                    Legerősebb szint
+                                    <p>(1-1.5 millió Ft környéke)</p>
+                                    <p></p>
+                                    <p></p>
+                                    <p></p>
+                                    <p></p>
+                                    <p></p>
+                                    <p></p>
+                                    <p></p>
+                                    <h4></h4>
+                                    <section>
+
+                                    </section>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
 
