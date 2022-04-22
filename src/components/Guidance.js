c4597791ffdb57c116714bbe609ca451966d4575
szlevi1998 <sz.levi1998@gmail.com>
2022-04-22 15:12:04 +0200

    Bit of change to the builder and guidance is in progress.

--- src/components/Guidance.js
+++ src/components/Guidance.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import NavBar from '../NavBar';
+import '../stylesheets/guidance.css';
+import {Container} from "react-bootstrap";
 
 function Guide() {
     return (
         <div>
             <NavBar/>
+
+            <h3 className='guidance-h3' >Útmutató</h3>
+            <Container className="container-md">
+                <h3 className="container-h3">Alap konfigok</h3>
+            </Container>
+
         </div>
     );
 }
--- src/components/PcBuilder.js
+++ src/components/PcBuilder.js
@@ -49,108 +49,115 @@ function PcBuilderPage() {
             <NavBar/>
             <h2 className='builder-h2'>Gép Összerakó</h2>
             <section className='table-content'>
-                <Table responsive='md'>
+                <Table>
                     <thead>
                     <tr>
                         <th className='component'>Komponens</th>
-                        <th>Termék neve</th>
-                        <th>Legolcsóbb ár</th>
-                        <th>Átlag ár</th>
-                        <th></th>
+                        <th className='product'>Termék neve</th>
+                        <th className='lowest-price'>Legolcsóbb ár</th>
+                        <th className='average-price'>Átlag ár</th>
+                        <th className='select'></th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
                         <td className='column'>Alaplap</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div>
-                                <a className='btn btn-primary add-button' onClick={navigateToMotherboards}>+
-                                    Hozzáadás</a>
+                            <a className='btn btn-primary add-button' onClick={navigateToMotherboards}>+ Hozzáadás</a>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
                         <td className='column'>Processzor</td>
-                        <td>
-                            <div>
-                                <a className='btn btn-primary add-button' onClick={navigateToProcessors}>+ Hozzáadás</a>
-                            </div>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><div>
+                            <a className='btn btn-primary add-button' onClick={navigateToProcessors}>+ Hozzáadás</a>
+                        </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
                         <td className='column'>Processzor hűtő</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div>
                                 <a className='btn btn-primary add-button' onClick={navigateToProcessorCoolers}>+
                                     Hozzáadás</a>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
                         <td className='column'>Memória</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div>
                                 <a className='btn btn-primary add-button' onClick={navigateToMemories}>+ Hozzáadás</a>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
                         <td className='column'>Videókártya</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div>
                                 <a className='btn btn-primary add-button' onClick={navigateToVideoCards}>+ Hozzáadás</a>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
                         <td className='column'>Tároló</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div>
                                 <a className='btn btn-primary add-button' onClick={navigateToStorages}>+ Hozzáadás</a>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
                         <td className='column'>Gépház</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div>
                                 <a className='btn btn-primary add-button' onClick={navigateToCases}>+ Hozzáadás</a>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
                         <td className='column'>Tápegység</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div>
                                 <a className='btn btn-primary add-button' onClick={navigateToPSUs}>+ Hozzáadás</a>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
                         <td className='column'>Monitor</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div>
                                 <a className='btn btn-primary add-button' onClick={navigateToMonitors}>+ Hozzáadás</a>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     </tbody>
                 </Table>
